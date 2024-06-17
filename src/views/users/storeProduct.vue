d697e2de0b136d1a58452537dec246c3a03ebc7b
thanhdo21d <114094731+thanvandung1204@users.noreply.github.com>
2024-06-17 09:47:55 +0700

    backup dovt

--- src/views/users/storeProduct.vue
+++ src/views/users/storeProduct.vue
@@ -1,75 +1,117 @@
 <template>
-    <a-card style="width: 100%">
-        <div class="row mb-3">
-            <div class="col-6">
-                <a-breadcrumb>
-                    <a-breadcrumb-item>Sản phẩm của Cửa hàng</a-breadcrumb-item>
-                </a-breadcrumb>
-            </div>
-            <div class="col-6 d-flex justify-content-end">
-                <a-button class="me-2">
-                    <router-link :to="{ name: 'product-type/them-moi' , params:{id :storeId2 } }">
-                    product type
+  <a-card style="width: 100%">
+    <div class="row mb-3">
+      <div class="col-6">
+        <a-breadcrumb>
+          <a-breadcrumb-item>Sản phẩm của Cửa hàng</a-breadcrumb-item>
+        </a-breadcrumb>
+      </div>
+      <div class="col-6 d-flex justify-content-end">
+        <a-button class="me-2">
+          <router-link
+            :to="{ name: 'product-type/them-moi', params: { id: storeId2 } }"
+          >
+            product type
+          </router-link>
+        </a-button>
+        <a-button type="primary" title="Thêm mới">
+          <router-link
+            :to="{ name: 'product/them-moi', params: { id: storeId2 } }"
+          >
+            <i class="fa-solid fa-plus"></i>
+          </router-link>
+        </a-button>
+      </div>
+    </div>
+    <div class="row">
+      <div class="col-12">
+        <a-table
+          :dataSource="users"
+          :columns="columns"
+          :scroll="{ x: 576 }"
+          :pagination="false"
+        >
+          <template #bodyCell="{ column, index, record }">
+            <template v-if="column.key === 'index'">
+              <span>{{ index + 1 }}</span>
+            </template>
+            <template v-if="column.key === 'userName'">
+              <span>{{ record.storeName }}</span>
+            </template>
+            <template v-if="column.key === 'imageSp'">
+              <img
+                :style="{ width: '50px !important' }"
+                :src="record.avatarProduct"
+                :alt="record.avatarProduct"
+              />
+            </template>
+            <template v-if="column.key === 'fullName'">
+              <span>{{ record.productName }}</span>
+            </template>
+            <template v-if="column.key === 'email'">
+              <span>{{ record.price }}</span>
+            </template>
+            <template v-if="column.key === 'action' && authStoreClaim !== null">
+              <a-space warp>
+                <router-link
+                  :to="{
+                    name: 'admin-product-edit',
+                    params: { id: record.id },
+                  }"
+                >
+                  <a-button
+                    type="dashed"
+                    class="me-2 text-primary"
+                    size="small"
+                    title="Sửa"
+                  >
+                    <i class="fa-solid fa-pen-to-square"></i>
+                  </a-button>
                 </router-link>
-
+              </a-space>
+              <router-link
+                :to="{
+                  name: 'admin-chi-tiet-san-pham',
+                  params: { id: record.productId },
+                }"
+              >
+                <a-button
+                  title="Khóa"
+                  type="dashed"
+                  size="small"
+                  shape=""
+                  class="me-2 text-warning"
+                  >xem
                 </a-button>
-                <a-button type="primary" title="Thêm mới">
-                    <router-link :to="{ name: 'product/them-moi' , params:{id :storeId2 } }">
-                        <i class="fa-solid fa-plus"></i>
-                    </router-link>
-                </a-button>
-
-            </div>
+              </router-link>
+              <a-popconfirm
+                title="Dữ liệu sẽ không thể phục hồi, bạn muốn xóa bản ghi này?"
+                ok-text="Yes"
+                cancel-text="No"
+                @confirm="confirmRemove(record.id)"
+              >
+                <a-button title="Xóa" type="dashed" size="small" shape="" danger
+                  ><i class="fa-solid fa-trash-can"></i
+                ></a-button>
+              </a-popconfirm>
+            </template>
+          </template>
+        </a-table>
+        <div class="col-12">
+          <a-pagination
+            @change="onChange"
+            v-model:current="pageParam.current"
+            :total="pageParam.totalRecord"
+            :pageSize="pageParam.pageSize"
+            :show-total="
+              (total, range) => `${range[0]}-${range[1]} of ${total} items`
+            "
+            class="mt-2 text-end"
+          />
         </div>
-        <div class="row">
-            <div class="col-12">
-                <a-table :dataSource="users" :columns="columns" :scroll="{ x: 576 }" :pagination=false>
-                    <template #bodyCell="{ column, index, record }">
-                        <template v-if="column.key === 'index'">
-                            <span>{{ index + 1 }}</span>
-                        </template>
-                        <template v-if="column.key === 'userName'">
-                            <span>{{ record.storeName }}</span>
-                        </template>
-                        <template v-if="column.key === 'imageSp'">
-                            <img :style="{ width: '50px !important' }" :src="record.avatarProduct"
-                                :alt="record.avatarProduct">
-                        </template>
-                        <template v-if="column.key === 'fullName'">
-                            <span>{{ record.productName }}</span>
-                        </template>
-                        <template v-if="column.key === 'email'">
-                            <span>{{ record.price }}</span>
-                        </template>
-                        <template v-if="column.key === 'action' && authStoreClaim !== null">
-                            <a-space warp>
-                                <router-link :to="{ name: 'admin-product-edit', params: { id: record.id } }">
-                                    <a-button type="dashed" class="me-2 text-primary" size="small" title="Sửa">
-                                        <i class="fa-solid fa-pen-to-square"></i>
-                                    </a-button>
-                                </router-link>
-                            </a-space>
-                            <router-link :to="{ name: 'admin-chi-tiet-san-pham', params: { id: record.productId } }">
-                                <a-button title="Khóa" type="dashed" size="small" shape="" class="me-2 text-warning">xem
-                                </a-button>
-                            </router-link>
-                            <a-popconfirm title="Dữ liệu sẽ không thể phục hồi, bạn muốn xóa bản ghi này?" ok-text="Yes"
-                                cancel-text="No" @confirm="confirmRemove(record.id)">
-                                <a-button title="Xóa" type="dashed" size="small" shape="" danger><i
-                                        class="fa-solid fa-trash-can"></i></a-button>
-                            </a-popconfirm>
-                        </template>
-                    </template>
-                </a-table>
-                <div class="col-12">
-                    <a-pagination @change="onChange" v-model:current="pageParam.current" :total=pageParam.totalRecord
-                        :pageSize="pageParam.pageSize"
-                        :show-total="(total, range) => `${range[0]}-${range[1]} of ${total} items`"
-                        class="mt-2 text-end" />
-                </div>
-            </div>
-        </div>
-    </a-card>
+      </div>
+    </div>
+  </a-card>
 </template>
 <script>
 import { defineComponent, ref, reactive } from "vue";
@@ -77,159 +119,171 @@ import { useRouter } from "vue-router";
 import { useRoute } from "vue-router";
 import { message } from "ant-design-vue";
 import { useMenu } from "../../stores/use-menu.js";
-import { onUpdated, onMounted } from 'vue'
-import ApiViewData from '../../api/ApiViewData.js';
-import ApiUser from '../../api/ApiUser.js';
+import { onUpdated, onMounted } from "vue";
+import ApiViewData from "../../api/ApiViewData.js";
+import ApiUser from "../../api/ApiUser.js";
 import { useAuthStore } from "../../stores/auth.store.js";
 import axios from "axios";
 export default defineComponent({
-    setup() {
-        useMenu().onSelectedKeys(["admin-users"]);
-        const authStoreClaim = ref(useAuthStore().user.roleClaimDetail);
-        const router = useRouter();
-        const route = useRoute();
-        const errors = ref([]);
-        const users = ref([]);
-        const storeId = route.params.id;
-        console.log(storeId, 'storeId')
-        const storeId2 = ref(route.params.id);
-        console.log(storeId2._value, 'storeId2')
+  setup() {
+    useMenu().onSelectedKeys(["admin-users"]);
+    const authStoreClaim = ref(useAuthStore().user.roleClaimDetail);
+    const router = useRouter();
+    const route = useRoute();
+    const errors = ref([]);
+    const users = ref([]);
+    const storeId = route.params.id;
+    console.log(storeId, "storeId");
+    const storeId2 = ref(route.params.id);
+    console.log(storeId2._value, "storeId2");
 
-        const pageParam = reactive({
-            current: (Object.keys(route.query).length > 0) ? route.query.PageNumber : 1,
-            pageNumber: (Object.keys(route.query).length > 0) ? route.query.PageNumber : 1,
-            pageSize: (Object.keys(route.query).length > 0) ? route.query.PageSize : 10,
-            totalRecord: 0,
-            userName: (Object.keys(route.query).length > 0) ? route.query.UserName : '',
-            statusFilter: false
-        });
-        const columns = [
-            {
-                title: "#",
-                key: "index",
-            },
-            {
-                title: "Tên cửa hàng",
-                dataIndex: "userName",
-                key: "userName",
-            },
-            {
-                title: "Ảnh sản phẩm",
-                dataIndex: "imageSp",
-                key: "imageSp",
-            },
-            {
-                title: "Tên sản phẩm",
-                dataIndex: "fullName",
-                key: "fullName",
-            },
-            {
-                title: "Giá",
-                key: "email",
-            },
+    const pageParam = reactive({
+      current: Object.keys(route.query).length > 0 ? route.query.PageNumber : 1,
+      pageNumber:
+        Object.keys(route.query).length > 0 ? route.query.PageNumber : 1,
+      pageSize: Object.keys(route.query).length > 0 ? route.query.PageSize : 10,
+      totalRecord: 0,
+      userName: Object.keys(route.query).length > 0 ? route.query.UserName : "",
+      statusFilter: false,
+    });
+    const columns = [
+      {
+        title: "#",
+        key: "index",
+      },
+      {
+        title: "Tên cửa hàng",
+        dataIndex: "userName",
+        key: "userName",
+      },
+      {
+        title: "Ảnh sản phẩm",
+        dataIndex: "imageSp",
+        key: "imageSp",
+      },
+      {
+        title: "Tên sản phẩm",
+        dataIndex: "fullName",
+        key: "fullName",
+      },
+      {
+        title: "Giá",
+        key: "email",
+      },
 
-            {
-                title: "Tác vụ",
-                key: "action",
-                fixed: "right",
-            },
-        ];
+      {
+        title: "Tác vụ",
+        key: "action",
+        fixed: "right",
+      },
+    ];
 
     const token = JSON.parse(localStorage.getItem("token"));
-        const getUsers = (args) => {
-            axios.get(`https://charismatic-friendship-production.up.railway.app/api/v1/management/${storeId2._value}/product/view`,  {
+    const getUsers = (args) => {
+      axios
+        .get(
+          `https://charismatic-friendship-production.up.railway.app/api/v1/management/${storeId2._value}/product/view`,
+          {
             headers: {
               Authorization: `Bearer ${token}`, // Thêm token vào headers
             },
-          }).then((response) => {
-                console.log(response.data.data, 'response')
-                users.value = response.data.data;
-            }).catch((error) => {
-                console.error(error)
-            })
-        };
-        const confirmRemove = (id) => {
-            ApiUser.DeleteById(id)
-                .then((response) => {
-                    console.log(response);
-                    if (response.status == 200) {
-                        message.success("Xóa thành công!");
-                        // router.push({ name: "admin-users" });
-                    }
-                    getUsers(pageParam);
-                })
-                .catch((error) => {
-                    message.error(error.message);
-                    if (error.response.data.hasOwnProperty('errors')) {
-                        errors.value = error.response.data.errors;
-                    } else {
-                        errors.value = error.response.data;
-                    }
-                });
-        };
-        const confirmBanned = (id) => {
-            ApiUser.BannedById(id).then((response) => {
-                if (response.status == 200) {
-                    message.success("Khóa thành công!");
-                } else {
-                    message.error("Lỗi! Tác vụ thực hiện không thành công.");
-                }
-                getUsers(pageParam);
-            })
-                .catch((error) => {
-                    message.error(error.message);
-                    if (error.response.data.hasOwnProperty('errors')) {
-                        errors.value = error.response.data.errors;
-                    } else {
-                        errors.value = error.response.data;
-                    }
-                });
-        };
-        //
-        onUpdated(() => {
-            //
-            if (Object.keys(route.query).length === 0) {
-                pageParam.current = (Object.keys(route.query).length > 0) ? route.query.PageNumber : 1;
-                pageParam.pageNumber = (Object.keys(route.query).length > 0) ? route.query.PageNumber : 1;
-                pageParam.pageSize = (Object.keys(route.query).length > 0) ? route.query.PageSize : 10;
-                pageParam.userName = (Object.keys(route.query).length > 0) ? route.query.UserName : '';
-                pageParam.statusFilter = true;
-                getUsers(pageParam);
-            }
+          }
+        )
+        .then((response) => {
+          console.log(response.data.data, "response");
+          users.value = response.data.data;
         })
-        onMounted(() => {
-            // chay lan dau tien
-            getUsers(pageParam);
+        .catch((error) => {
+          console.error(error);
+        });
+    };
+    const confirmRemove = (id) => {
+      ApiUser.DeleteById(id)
+        .then((response) => {
+          console.log(response);
+          if (response.status == 200) {
+            message.success("Xóa thành công!");
+            // router.push({ name: "admin-users" });
+          }
+          getUsers(pageParam);
         })
-        //
-        function onChange(page, pageSize) {
-            pageParam.pageNumber = page;
-            pageParam.pageSize = pageSize;
-            //
-            pageParam.statusFilter = true;
-            getUsers(pageParam);
-        }
-        //
-        const clickFrmFilter = (event) => {
-            pageParam.statusFilter = true;
-            getUsers(pageParam);
-        }
-        //
-        return {
-            route,
-            router,
-            authStoreClaim,
-            errors,
-            users,
-            columns,
-            pageParam,
-            storeId2,
-            onChange,
-            clickFrmFilter,
-            confirmRemove,
-            confirmBanned
-        };
-        //
+        .catch((error) => {
+          message.error(error.message);
+          if (error.response.data.hasOwnProperty("errors")) {
+            errors.value = error.response.data.errors;
+          } else {
+            errors.value = error.response.data;
+          }
+        });
+    };
+    const confirmBanned = (id) => {
+      ApiUser.BannedById(id)
+        .then((response) => {
+          if (response.status == 200) {
+            message.success("Khóa thành công!");
+          } else {
+            message.error("Lỗi! Tác vụ thực hiện không thành công.");
+          }
+          getUsers(pageParam);
+        })
+        .catch((error) => {
+          message.error(error.message);
+          if (error.response.data.hasOwnProperty("errors")) {
+            errors.value = error.response.data.errors;
+          } else {
+            errors.value = error.response.data;
+          }
+        });
+    };
+    //
+    onUpdated(() => {
+      //
+      if (Object.keys(route.query).length === 0) {
+        pageParam.current =
+          Object.keys(route.query).length > 0 ? route.query.PageNumber : 1;
+        pageParam.pageNumber =
+          Object.keys(route.query).length > 0 ? route.query.PageNumber : 1;
+        pageParam.pageSize =
+          Object.keys(route.query).length > 0 ? route.query.PageSize : 10;
+        pageParam.userName =
+          Object.keys(route.query).length > 0 ? route.query.UserName : "";
+        pageParam.statusFilter = true;
+        getUsers(pageParam);
+      }
+    });
+    onMounted(() => {
+      // chay lan dau tien
+      getUsers(pageParam);
+    });
+    //
+    function onChange(page, pageSize) {
+      pageParam.pageNumber = page;
+      pageParam.pageSize = pageSize;
+      //
+      pageParam.statusFilter = true;
+      getUsers(pageParam);
     }
+    //
+    const clickFrmFilter = (event) => {
+      pageParam.statusFilter = true;
+      getUsers(pageParam);
+    };
+    //
+    return {
+      route,
+      router,
+      authStoreClaim,
+      errors,
+      users,
+      columns,
+      pageParam,
+      storeId2,
+      onChange,
+      clickFrmFilter,
+      confirmRemove,
+      confirmBanned,
+    };
+    //
+  },
 });
 </script>
